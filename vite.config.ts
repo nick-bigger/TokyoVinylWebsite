8b7c7d70c9cf111beca19f2e3a27c2ae3b3d97c5
nick.bigger <nick.bigger@gmail.com>
2025-07-29 19:35:06 -0500

    Fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/TokyoVinylBand/",
+  base: "/TokyoVinylWebsite/",
   plugins: [react()],
   resolve: {
     alias: {
